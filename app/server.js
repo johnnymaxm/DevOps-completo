7624f4af15da397af15d9baa3807d9fc6eecdba0
johnnymax <johnnnymax_10@hotmail.com>
2026-03-18 18:07:27 -0300

    Adicionando alteraçoes

--- app/server.js
+++ app/server.js
@@ -1,10 +1,24 @@
 const express = require("express");
+const client = require("prom-client");
+
 const app = express();
 
+// 🔥 Coleta métricas padrão (CPU, memória, etc)
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+// rota principal
 app.get("/", (req, res) => {
-  res.json({ status: "ok", message: "Projeto DevOps" });
+  res.json({ message: "DevOps" });
 });
 
+// 📊 rota de métricas (IMPORTANTE)
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
+// start server
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
